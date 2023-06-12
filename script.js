76cace3c3e7f55a96c4dd262f41b2886658d5018
sandxz <sandronesta201@gmail.com>
2023-06-12 20:27:16 +0800

    changes

--- script.js
+++ script.js
@@ -1,22 +1,28 @@
 var featureIndex = 0;
     var features = [
       {
-        image: 'img/det-image.png',
-        title: 'Find Nearby Laundry',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis id scelerisque turpis.'
+        image: 'img/det5.png',
+        title: 'Coming Soons',
+        description: 'Our app is still under development the rest of the features coming soon and are in progress of working still.'
       },
       {
-        image: 'img/det-image3.png',
+        image: 'img/det3.png',
+        title: 'Find Nearby Laundry',
+        description: 'You can find nearby laundry by geolocation system that we have implemented.'
+      },
+      {
+        image: 'img/det2.png',
         title: 'Find Nearby Laundry',
         description: 'Vestibulum malesuada dolor ac cursus sollicitudin. Vivamus rutrum tincidunt nulla, eu volutpat neque consectetur et.'
       },
       {
-        image: 'img/det-image2.png',
+        image: 'img/det4.png',
         title: 'Find Nearby Laundry',
         description: 'Vestibulum malesuada dolor ac cursus sollicitudin. Vivamus rutrum tincidunt nulla, eu volutpat neque consectetur et.'
-      }
+      },
+      
     ];
-    var slideInterval = setInterval(showNextFeature, 5000); // Auto slide interval in milliseconds (5 seconds)
+    var slideInterval = setInterval(showNextFeature, 99999); // Auto slide interval in milliseconds (5 seconds)
 
     function showNextFeature() {
       var featureImage = document.getElementById('featureImage');
